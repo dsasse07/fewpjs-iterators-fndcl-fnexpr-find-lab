a0bd9d013a135fa3060887c6bfe7d9dab888c0b3
Daniel Sasse <dsasse07@gmail.com>
2021-02-05 16:35:05 -0500

    Done

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = array => {
+  let winningGame = array.find( game => game.result === "W" )
+  if (winningGame) {
+    return winningGame.year
+  } else {
+    return undefined
+  }
+
+}
